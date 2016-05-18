84c6f6e7e65efec5274b12e5cf886d5b38348158
wknott <wyoyo7@gmail.com>
2016-05-18 20:46:37 +0200

    plik glowny, wykonuje losowanie i 4 metryki

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+#!/usr/bin/env python
+
 from similaritymeasures import Similarity
-
-def main():
-
-	""" main function to create Similarity class instance and get use of it """
-	
-	measures = Similarity()
-
-	print (measures.euclidean_distance([0,3,4,5],[7,6,3,-1]))
-	print (measures.jaccard_similarity([0,1,2,5,6],[0,2,3,5,7]))
-
-if __name__ == "__main__":
-	main()
+import pandas as pd
+import random
+nofrow=10-1
+nofcol=10
+df = pd.read_csv('dane.csv')
+row1=random.randint(0,nofrow)
+row2=random.randint(0,nofrow)
+while row1==row2:
+    row2=random.randint(0,nofrow)
+ncol=random.randint(1,nofcol)
+zakres=range(0,nofcol)
+col=random.sample(zakres,ncol)
+x = []
+y = []
+i=0
+while i<ncol:
+    x.insert(i,df.iat[row1,col[i]])
+    y.insert(i,df.iat[row2,col[i]])
+    i += 1
+print(x)
+print(y)
+measures = Similarity()
+print (measures.euclidean_distance(x,y))
+print (measures.jaccard_similarity(x,y))
+print (measures.cosine_similarity(x,y))
+print (measures.manhattan_distance(x,y))
+#print (measures.minkowski_distance(x,y,3))
